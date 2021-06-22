c892e3be336fcce4eb1c5f105dca592b100d6e68
Shist <shist.pupust@mail.ru>
2021-06-22 08:25:01 +0300

    Refactored some project files...

--- app/src/main/java/io/navendra/retrofitkotlindeferred/ui/MainActivity.kt
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/ui/MainActivity.kt
@@ -19,30 +19,23 @@ class MainActivity : AppCompatActivity() {
 
         val service = SportNewsFactory.SPORT_NEWS_API
 
-        // Getting News from News Content API
         // RecycleView вместо ScrollView
         // Два фрагмента: для блока новостей и для деталей одной какой-то новости (при нажатии на неё)
-
 
         GlobalScope.launch(Dispatchers.Main) {
             try {
                 val userRequest = service.getNews()
-
-                val response = userRequest
-                Log.d("MyLog", "response: ${response.items.size}")
-//                if(response.isSuccessful){
-//                    Log.d("MyLog", "Successful start logging...")
-//                    Log.d("MyLog", response.body().toString())
-//                }else{
-//                    Log.d("MyLog", "Failure start logging...")
-//                    Log.d("MyLog", response.errorBody().toString())
-//                }
+                if(userRequest.items.isNotEmpty()){
+                    Log.d("MyLog", "Successful start logging...")
+                    Log.d("MyLog", "response: ${userRequest.items.size} items")
+                    for (i in userRequest.items.indices)
+                        Log.d("MyLog", userRequest.items[i].toString())
+                }else{
+                    Log.d("MyLog", "Failure while getting response...")
+                }
             } catch (e: Throwable){
                 Log.d("MyLog", "Failure...", e)
             }
         }
-
     }
-
-
 }
