513729a19743329134d92fe2834a83faf6e7754d
Shist <shist.pupust@mail.ru>
2021-06-22 08:06:22 +0300

    Deleted authInterceptor in RetrofitFactory

--- app/src/main/java/io/navendra/retrofitkotlindeferred/service/RetrofitFactory.kt
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/service/RetrofitFactory.kt
@@ -1,28 +1,11 @@
 package io.navendra.retrofitkotlindeferred.service
-import io.navendra.retrofitkotlindeferred.AppConstants
 import io.navendra.retrofitkotlindeferred.BuildConfig
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitFactory{
-
-    // Убрать authInterceptor (вроде как он не нужен)
-    private val authInterceptor = Interceptor {chain->
-        val newUrl = chain.request().url()
-                .newBuilder()
-                .addQueryParameter("api_key", AppConstants.tmdbApiKey)
-                .build()
-
-        val newRequest = chain.request()
-                .newBuilder()
-                .url(newUrl)
-                .build()
-
-        chain.proceed(newRequest)
-    }
 
     private val loggingInterceptor =  HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
@@ -32,13 +15,11 @@ object RetrofitFactory{
     private val client =
         if(BuildConfig.DEBUG){
              OkHttpClient().newBuilder()
-                    .addInterceptor(authInterceptor)
                     .addInterceptor(loggingInterceptor)
                     .build()
         }else{
             OkHttpClient().newBuilder()
                     .addInterceptor(loggingInterceptor)
-                    .addInterceptor(authInterceptor)
                     .build()
         }
 
@@ -46,7 +27,6 @@ object RetrofitFactory{
             .client(client)
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
-//            .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .build()
 
 }
