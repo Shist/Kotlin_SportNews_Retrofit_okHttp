1f8626ef994172716f9a0050165dc38a0b302e51
Agathon006 <agaton615@gmail.com>
2021-06-28 11:24:30 +0300

    Task_try_2

--- app/src/main/java/io/navendra/retrofitkotlindeferred/ui/MainActivity.kt
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/ui/MainActivity.kt
@@ -11,31 +11,11 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    // Создать класс ViewModel - промежуточный между MainActivity и UI
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val service = SportNewsFactory.SPORT_NEWS_API
-
-        // RecycleView вместо ScrollView
-        // Два фрагмента: для блока новостей и для деталей одной какой-то новости (при нажатии на неё)
-
-        GlobalScope.launch(Dispatchers.Main) {
-            try {
-                val userRequest = service.getNews()
-                if(userRequest.items.isNotEmpty()){
-                    Log.d("MyLog", "Successful start logging...")
-                    Log.d("MyLog", "response: ${userRequest.items.size} items")
-                    for (i in userRequest.items.indices)
-                        Log.d("MyLog", userRequest.items[i].toString())
-                }else{
-                    Log.d("MyLog", "Failure while getting response...")
-                }
-            } catch (e: Throwable){
-                Log.d("MyLog", "Failure...", e)
-            }
-        }
     }
+
+
+    //добавить сюда фрагмент, в нём создать адаптер и заполнить его RecycleView
 }
