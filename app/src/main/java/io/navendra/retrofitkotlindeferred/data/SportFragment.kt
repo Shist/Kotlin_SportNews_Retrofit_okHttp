800e0858a467f0825fcf544ef264818d60fe072e
Shist <shist.pupust@mail.ru>
2021-06-28 11:54:26 +0300

    Added SportFragment, SportViewModel, sport_fragment_layout.xml

---
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/data/SportFragment.kt
@@ -0,0 +1,40 @@
+package io.navendra.retrofitkotlindeferred.data
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import io.navendra.retrofitkotlindeferred.R
+
+class SportFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SportFragment()
+    }
+
+    private lateinit var viewModel: SportViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.sport_fragment_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.news.observe(this.viewLifecycleOwner, Observer {})
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SportViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
+
+// Два фрагмента: для блока новостей и для деталей одной какой-то новости (при нажатии на неё)
