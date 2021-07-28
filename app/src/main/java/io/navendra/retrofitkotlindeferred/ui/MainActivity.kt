bafc0fe394298e798f2759a122a6ffda6329cf28
Shist <shist.pupust@mail.ru>
2021-07-28 13:56:33 +0300

    Added View Binding components for MainActivity

--- app/src/main/java/io/navendra/retrofitkotlindeferred/ui/MainActivity.kt
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/ui/MainActivity.kt
@@ -3,13 +3,19 @@ package io.navendra.retrofitkotlindeferred.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.navendra.retrofitkotlindeferred.R
+import io.navendra.retrofitkotlindeferred.databinding.ActivityMainBinding
 import io.navendra.retrofitkotlindeferred.model.NewsItem
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, NewsListFragment())
