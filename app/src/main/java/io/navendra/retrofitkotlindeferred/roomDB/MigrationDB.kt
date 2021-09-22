6828bff091c2f6910a9b5910ef5a5bba4488cf59
Shist <shist.pupust@mail.ru>
2021-09-22 17:03:47 +0300

    Migration 1_2 has been corrected: Headline --> HeadLine as needed in databases

--- app/src/main/java/io/navendra/retrofitkotlindeferred/roomDB/MigrationDB.kt
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/roomDB/MigrationDB.kt
@@ -11,26 +11,26 @@ object MigrationDB {
                     "body TEXT NOT NULL, " +
                     "createdAt INTEGER NOT NULL, " +
                     "context TEXT NOT NULL, " +
-                    "shortHeadline TEXT NOT NULL);")
+                    "shortHeadLine TEXT NOT NULL);")
             db.execSQL("INSERT INTO " +
-                    "itemsWithDetails(itemId, body, createdAt, context, shortHeadline) " +
-                    "SELECT itemId, body, createdAt, context, shortHeadline FROM items;")
+                    "itemsWithDetails(itemId, body, createdAt, context, shortHeadLine) " +
+                    "SELECT itemId, body, createdAt, context, shortHeadLine FROM items;")
             db.execSQL("CREATE TABLE IF NOT EXISTS itemsTemp (" +
                     "itemId TEXT PRIMARY KEY NOT NULL, " +
                     "altText TEXT NOT NULL, " +
                     "createdAt INTEGER NOT NULL, " +
                     "context TEXT NOT NULL, " +
-                    "shortHeadline TEXT NOT NULL);")
+                    "shortHeadLine TEXT NOT NULL);")
             db.execSQL("INSERT INTO " +
-                    "itemsTemp(itemId, altText, createdAt, context, shortHeadline) " +
-                    "SELECT itemId, altText, createdAt, context, shortHeadline FROM items;")
+                    "itemsTemp(itemId, altText, createdAt, context, shortHeadLine) " +
+                    "SELECT itemId, altText, createdAt, context, shortHeadLine FROM items;")
             db.execSQL("DROP TABLE IF EXISTS items;")
             db.execSQL("CREATE TABLE IF NOT EXISTS items (" +
                     "itemId TEXT PRIMARY KEY NOT NULL, " +
                     "altText TEXT NOT NULL, " +
                     "createdAt INTEGER NOT NULL, " +
                     "context TEXT NOT NULL, " +
-                    "shortHeadline TEXT NOT NULL);")
+                    "shortHeadLine TEXT NOT NULL);")
             db.execSQL("INSERT INTO items SELECT * FROM itemsTemp;")
             db.execSQL("DROP TABLE itemsTemp;")
         }
