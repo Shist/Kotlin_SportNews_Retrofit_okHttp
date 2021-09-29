ad7cc7df74ec69737fb2e61a1e0e9c250af66444
Shist <shist.pupust@mail.ru>
2021-09-29 16:45:41 +0300

    Deleted useless imports

--- app/src/main/java/io/navendra/retrofitkotlindeferred/ui/viewModel/NewsPageViewModel.kt
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/ui/viewModel/NewsPageViewModel.kt
@@ -1,23 +1,17 @@
 package io.navendra.retrofitkotlindeferred.ui.viewModel
 
 import android.app.Application
-import android.content.Context
-import android.net.ConnectivityManager
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import io.navendra.retrofitkotlindeferred.roomDB.entities.newsItemDetails.NewsItemDetailsTable
-import io.navendra.retrofitkotlindeferred.ui.fragments.NewsPageFragment
-import io.navendra.retrofitkotlindeferred.ui.repository.LoadState
 import io.navendra.retrofitkotlindeferred.ui.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.koin.core.parameter.parametersOf
 import org.koin.core.qualifier.named
-import java.io.IOException
 
 class NewsPageViewModel(application: Application, itemID: String) :
     AndroidViewModel(application), KoinComponent {
