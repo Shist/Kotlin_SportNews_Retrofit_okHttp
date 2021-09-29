28f28ca6d2c082afa0e712f3a935b85b6a617f33
Shist <shist.pupust@mail.ru>
2021-09-29 16:18:07 +0300

    Added try-catch for loading item details while No Internet

--- app/src/main/java/io/navendra/retrofitkotlindeferred/ui/viewModel/NewsPageViewModel.kt
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/ui/viewModel/NewsPageViewModel.kt
@@ -1,15 +1,20 @@
 package io.navendra.retrofitkotlindeferred.ui.viewModel
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import io.navendra.retrofitkotlindeferred.roomDB.entities.newsItemDetails.NewsItemDetailsTable
+import io.navendra.retrofitkotlindeferred.ui.repository.LoadState
 import io.navendra.retrofitkotlindeferred.ui.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.io.IOException
 
 class NewsPageViewModel(application: Application, itemID: String) :
     AndroidViewModel(application), KoinComponent {
@@ -20,7 +25,16 @@ class NewsPageViewModel(application: Application, itemID: String) :
 
     fun loadData(itemID: String) {
         viewModelScope.launch {
-            newsRepository.loadNewsItemDetailsByID(itemID)
+            try {
+                newsRepository.loadNewsItemDetailsByID(itemID)
+            } catch (e: Throwable) {
+//                if (!isConnectedToInternet() && e is IOException) {
+//                    state.value = LoadState.INTERNET_ERROR
+//                }
+//                else {
+//                    state.value = LoadState.UNKNOWN_ERROR
+//                }
+            }
         }
     }
 
