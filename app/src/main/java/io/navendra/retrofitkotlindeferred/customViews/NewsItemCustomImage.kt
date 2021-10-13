145a2642a4fe0f511e0b3d591857c31e37ab77e9
Agathon006 <agaton615@gmail.com>
2021-10-13 15:21:58 +0300

    Custom view - added universal function for any n-angle

--- app/src/main/java/io/navendra/retrofitkotlindeferred/customViews/NewsItemCustomImage.kt
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/customViews/NewsItemCustomImage.kt
@@ -2,82 +2,80 @@ package io.navendra.retrofitkotlindeferred.customViews
 
 import android.content.Context
 import android.graphics.*
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
 import android.util.AttributeSet
-import android.widget.ImageView
 import android.graphics.Path.FillType
-import android.util.Log
 
 
 class NewsItemCustomImage(context: Context?, attrs: AttributeSet?) :
     androidx.appcompat.widget.AppCompatImageView(context!!, attrs) {
 
+    private val paint = Paint()
+
     override  fun  onDraw (canvas: Canvas ) {
+
+        val n = 9
+        drawNAngle(canvas, n)
 
         super .onDraw (canvas)
 
-        drawPentagon(canvas)
-
     }
 
-    private fun drawPentagon(canvas: Canvas) {
+    private fun drawNAngle(canvas: Canvas, n: Int) {
 
-        val paint = Paint()
+        val imageWidth = canvas.width
+        val imageHeight = canvas.height
 
-        val pentagonWidth = canvas.width
-        val pentagonHeight = canvas.height
+        val perimeter = 2*imageWidth + 2*imageHeight
 
-        //pentagonWidth = 518
-        //pentagonHeight = 291
+        val anglesDistance = perimeter/n
 
-        paint.strokeWidth = 4F
+        //imageWidth = 518
+        //imageHeight = 291
+        //perimeter = 1618
+        //angles_distance = 323.6 (for pentagon)
+
         paint.color = Color.WHITE
-        paint.style = Paint.Style.FILL_AND_STROKE
+        paint.style = Paint.Style.FILL
         paint.isAntiAlias = true
 
-        val aLeftTop = Point(0, 0)
-        val bLeftTop = Point(pentagonWidth/2, 0)
-        val cLeftTop = Point(0, pentagonHeight/3)
-
-        val aLeftBottom = Point(0, pentagonHeight*1)
-        val bLeftBottom = Point(0, pentagonHeight/3)
-        val cLeftBottom = Point(pentagonWidth/3, pentagonHeight*1)
-
-        val aRightTop = Point(pentagonWidth*1, 0)
-        val bRightTop = Point(pentagonWidth*1, pentagonHeight/3)
-        val cRightTop = Point(pentagonWidth/2, 0)
-
-        val aRightBottom = Point(pentagonWidth*1, pentagonHeight*1)
-        val bRightBottom = Point(pentagonWidth*2/3, pentagonHeight*1)
-        val cRightBottom = Point(pentagonWidth*1, pentagonHeight/3)
+        var currentSum = imageWidth/2
+        var x = 0
+        var y = 0
 
         val path = Path()
         path.fillType = FillType.EVEN_ODD
 
-        path.moveTo(aLeftTop.x.toFloat(), aLeftTop.y.toFloat())
-        path.lineTo(bLeftTop.x.toFloat(), bLeftTop.y.toFloat())
-        path.lineTo(cLeftTop.x.toFloat(), cLeftTop.y.toFloat())
-        path.lineTo(aLeftTop.x.toFloat(), aLeftTop.y.toFloat())
-
-        path.moveTo(aLeftBottom.x.toFloat(), aLeftBottom.y.toFloat())
-        path.lineTo(bLeftBottom.x.toFloat(), bLeftBottom.y.toFloat())
-        path.lineTo(cLeftBottom.x.toFloat(), cLeftBottom.y.toFloat())
-        path.lineTo(aLeftBottom.x.toFloat(), aLeftBottom.y.toFloat())
-
-        path.moveTo(aRightTop.x.toFloat(), aRightTop.y.toFloat())
-        path.lineTo(bRightTop.x.toFloat(), bRightTop.y.toFloat())
-        path.lineTo(cRightTop.x.toFloat(), cRightTop.y.toFloat())
-        path.lineTo(aRightTop.x.toFloat(), aRightTop.y.toFloat())
-
-        path.moveTo(aRightBottom.x.toFloat(), aRightBottom.y.toFloat())
-        path.lineTo(bRightBottom.x.toFloat(), bRightBottom.y.toFloat())
-        path.lineTo(cRightBottom.x.toFloat(), cRightBottom.y.toFloat())
-        path.lineTo(aRightBottom.x.toFloat(), aRightBottom.y.toFloat())
+        path.moveTo(imageWidth/2.toFloat(), 0.toFloat())
+        for (i in 1..n) {
+            currentSum += anglesDistance
+            if (currentSum > perimeter) {
+                currentSum -= perimeter
+            }
+            if (currentSum in 0..imageWidth) {
+                x = currentSum
+                y = 0
+            }
+            if (currentSum in imageWidth..(imageWidth+imageHeight)) {
+                x = imageWidth
+                y = currentSum - imageWidth
+            }
+            if (currentSum in (imageWidth+imageHeight)..(2*imageWidth+imageHeight)) {
+                x = imageWidth-(currentSum-(imageWidth+imageHeight))
+                y = imageHeight
+            }
+            if (currentSum in (2*imageWidth+imageHeight)..perimeter) {
+                x = 0
+                y = imageHeight-(currentSum-(2*imageWidth+imageHeight))
+            }
+            path.lineTo(
+                x.toFloat(),
+                y.toFloat()
+            )
+        }
 
         path.close()
 
-        canvas.drawPath(path, paint)
+        canvas.clipPath(path)
 
     }
 
