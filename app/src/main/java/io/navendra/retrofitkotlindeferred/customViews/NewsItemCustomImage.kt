0f5c0d3f69d27887e62a537ffd128ef0d6729008
Agathon006 <agaton615@gmail.com>
2021-10-19 17:26:13 +0300

    Passing parameter from xml to class was changed by declare styleable...

--- app/src/main/java/io/navendra/retrofitkotlindeferred/customViews/NewsItemCustomImage.kt
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/customViews/NewsItemCustomImage.kt
@@ -4,21 +4,26 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.graphics.Path.FillType
-
+import androidx.appcompat.widget.AppCompatImageView
+import io.navendra.retrofitkotlindeferred.R
+import kotlin.math.cos
+import kotlin.math.sin
 
 class NewsItemCustomImage(context: Context?, attrs: AttributeSet?) :
-    androidx.appcompat.widget.AppCompatImageView(context!!, attrs) {
+    AppCompatImageView(context!!, attrs) {
 
     // четные штуки - 6 (картинка слева, текст - справа)
     // нечетные штуки - 8 (картинска справа, текст - слева)
     // у recycle-а (adapter-а) нужно будет посмотреть, как делать ViewType
 
     private val paint = Paint()
-    private val n =
-        resources.getInteger(io.navendra.retrofitkotlindeferred.R.integer.n_for_angles_number)
+    private val typedArray =
+        context?.obtainStyledAttributes(attrs,R.styleable.NewsItemCustomImage)
+    private val anglesNumber =
+        typedArray?.getInteger(R.styleable.NewsItemCustomImage_attrAnglesNumber, 4)
 
     override  fun  onDraw (canvas: Canvas ) {
-        clip(canvas, n)
+        clip(canvas, anglesNumber!!)
         super .onDraw (canvas)
     }
 
@@ -27,7 +32,7 @@ class NewsItemCustomImage(context: Context?, attrs: AttributeSet?) :
 
     private val path = Path()
 
-    private fun clip(canvas: Canvas, n: Int) {
+    private fun clip(canvas: Canvas, anglesNumber: Int) {
 
         val imageWidth = canvas.width
         val imageHeight = canvas.height
@@ -38,15 +43,11 @@ class NewsItemCustomImage(context: Context?, attrs: AttributeSet?) :
 
         // Вынести за функцию, но как-то передать туда ширину и высоту
         for (i in 0..359) {
-            ellipseX[i] = (imageWidth/2) + (imageWidth/2)*kotlin.math.cos(Math.toRadians(i.toDouble()))
-            ellipseY[i] = (imageHeight/2) + (imageHeight/2)*kotlin.math.sin(Math.toRadians(i.toDouble()))
+            ellipseX[i] = (imageWidth/2) + (imageWidth/2)* cos(Math.toRadians(i.toDouble()))
+            ellipseY[i] = (imageHeight/2) + (imageHeight/2)* sin(Math.toRadians(i.toDouble()))
         }
 
-        var figureAngle = 360/n
-
-        if (figureAngle > 360) {
-            figureAngle = 360
-        }
+        val figureAngle = 360/anglesNumber
 
         var figureAnglePointX = 0
         var figureAnglePointY = 0
@@ -60,7 +61,7 @@ class NewsItemCustomImage(context: Context?, attrs: AttributeSet?) :
             ellipseY[269].toInt().toFloat()
         )
 
-        for (i in 1..n) {
+        for (i in 1..anglesNumber) {
             if (figureAngle*i+270-1 <= 359) {
                 figureAnglePointX = ellipseX[figureAngle * i + 270 - 1].toInt()
                 figureAnglePointY = ellipseY[figureAngle * i + 270 - 1].toInt()
