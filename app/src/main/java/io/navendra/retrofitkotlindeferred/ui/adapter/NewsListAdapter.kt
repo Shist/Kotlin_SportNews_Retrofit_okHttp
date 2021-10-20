c642bae74e296868458d0db64f6b7323917a0624
Shist <shist.pupust@mail.ru>
2021-10-20 14:26:52 +0300

    Working on Adapter with 2 View Types...

--- app/src/main/java/io/navendra/retrofitkotlindeferred/ui/adapter/NewsListAdapter.kt
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/ui/adapter/NewsListAdapter.kt
@@ -8,18 +8,17 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import io.navendra.retrofitkotlindeferred.R
 import io.navendra.retrofitkotlindeferred.databinding.NewsItemOddBinding
 import io.navendra.retrofitkotlindeferred.databinding.NewsItemEvenBinding
 import io.navendra.retrofitkotlindeferred.roomDB.entities.newsItem.NewsItemTable
 
-// Мы не можем иметь два типа штуки сразу, но нам нужны обе
 class SportAdapter(private val clickListener: (NewsItemTable)-> Unit) :
-    ListAdapter<NewsItemTable, SportAdapter.OddViewHolder>(SportNewsDiffCallback()),
-    ListAdapter<NewsItemTable, SportAdapter.EvenViewHolder>(SportNewsDiffCallback()) {
+    ListAdapter<NewsItemTable, RecyclerView.ViewHolder>(SportNewsDiffCallback()) {
 
     companion object {
-        private const val TYPE_ODD = 1
-        private const val TYPE_EVEN = 2
+        private const val TYPE_ODD = 0
+        private const val TYPE_EVEN = 1
     }
 
     class OddViewHolder(oddItemBinding: NewsItemOddBinding): RecyclerView.ViewHolder(oddItemBinding.root) {
@@ -34,38 +33,35 @@ class SportAdapter(private val clickListener: (NewsItemTable)-> Unit) :
         val altText: TextView = evenItemBinding.altText
     }
 
-    // Проблема связана с верхней проблемой: мы не знаем, какой из двух типов используется классом и соответственно какой нужен
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = when (viewType) {
-        TYPE_ODD -> NewsItemOddBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        TYPE_EVEN -> NewsItemEvenBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        TYPE_ODD -> OddViewHolder(NewsItemOddBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false))
+        TYPE_EVEN -> EvenViewHolder(NewsItemEvenBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false))
         else -> throw IllegalArgumentException()
     }
 
     override fun getItemViewType(position: Int): Int =
         when (position % 2) {
-            1 -> TYPE_ODD
-            2 -> TYPE_EVEN
+            0 -> TYPE_ODD
+            1 -> TYPE_EVEN
             else -> throw IllegalArgumentException()
         }
 
-    override fun onBindViewHolder(oddHolder: OddViewHolder,
-                                  evenHolder: EvenViewHolder,
-                                  position: Int) {
-        val listItem = getItem(position) // Опять таки, он не понимает, который айтем получать из двух
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val listItem = getItem(position)
+
+        holder.itemView.setOnClickListener {
+            clickListener(listItem)
+        }
 
         when (getItemViewType(position)) {
             TYPE_ODD -> {
-                oddHolder.itemView.setOnClickListener {
-                    clickListener(listItem)
-                }
-                Picasso.get().load(listItem.context).into(oddHolder.image)
-                oddHolder.headline.text = listItem.shortHeadline
+                Picasso.get().load(listItem.context).into(holder.itemView.findViewById() image)
+                holder.itemViewType. .headline.text = listItem.shortHeadline
                 oddHolder.altText.text = listItem.altText
             }
             TYPE_EVEN -> {
-                evenHolder.itemView.setOnClickListener {
-                    clickListener(listItem)
-                }
                 Picasso.get().load(listItem.context).into(evenHolder.image)
                 evenHolder.headline.text = listItem.shortHeadline
                 evenHolder.altText.text = listItem.altText
