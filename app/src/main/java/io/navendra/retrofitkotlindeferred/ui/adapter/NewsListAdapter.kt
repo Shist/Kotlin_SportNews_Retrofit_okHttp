9e93bb827f442692a97f32935377c158f317f174
Shist <shist.pupust@mail.ru>
2021-10-20 15:59:44 +0300

    Now, we have only one class for holder (with overload constructor)

--- app/src/main/java/io/navendra/retrofitkotlindeferred/ui/adapter/NewsListAdapter.kt
+++ app/src/main/java/io/navendra/retrofitkotlindeferred/ui/adapter/NewsListAdapter.kt
@@ -7,36 +7,49 @@ import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 import com.squareup.picasso.Picasso
 import io.navendra.retrofitkotlindeferred.databinding.NewsItemOddBinding
 import io.navendra.retrofitkotlindeferred.databinding.NewsItemEvenBinding
 import io.navendra.retrofitkotlindeferred.roomDB.entities.newsItem.NewsItemTable
 
 class SportAdapter(private val clickListener: (NewsItemTable)-> Unit) :
-    ListAdapter<NewsItemTable, RecyclerView.ViewHolder>(SportNewsDiffCallback()) {
+    ListAdapter<NewsItemTable, SportAdapter.ItemViewHolder>(SportNewsDiffCallback()) {
 
     companion object {
         private const val TYPE_ODD = 0
         private const val TYPE_EVEN = 1
     }
 
-    class OddViewHolder(oddItemBinding: NewsItemOddBinding): RecyclerView.ViewHolder(oddItemBinding.root) {
-        val image: ImageView = oddItemBinding.img
-        val headline: TextView = oddItemBinding.headline
-        val altText: TextView = oddItemBinding.altText
-    }
+    class ItemViewHolder(bindType: ViewBinding): RecyclerView.ViewHolder(bindType.root) {
 
-    class EvenViewHolder(evenItemBinding: NewsItemEvenBinding): RecyclerView.ViewHolder(evenItemBinding.root) {
-        val image: ImageView = evenItemBinding.img
-        val headline: TextView = evenItemBinding.headline
-        val altText: TextView = evenItemBinding.altText
+        lateinit var image: ImageView
+        lateinit var headline: TextView
+        lateinit var altText: TextView
+
+        constructor(bindType: ViewBinding, typeNum: Int) : this(bindType) {
+            when (typeNum) {
+                0 -> {
+                    bindType as NewsItemOddBinding
+                    image = bindType.img
+                    headline = bindType.headline
+                    altText = bindType.altText
+                }
+                1 -> {
+                    bindType as NewsItemEvenBinding
+                    image = bindType.img
+                    headline = bindType.headline
+                    altText = bindType.altText
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = when (viewType) {
-        TYPE_ODD -> OddViewHolder(NewsItemOddBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false))
-        TYPE_EVEN -> EvenViewHolder(NewsItemEvenBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false))
+        TYPE_ODD -> ItemViewHolder(NewsItemOddBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false), 0)
+        TYPE_EVEN -> ItemViewHolder(NewsItemEvenBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false), 1)
         else -> throw IllegalArgumentException()
     }
 
@@ -47,7 +60,7 @@ class SportAdapter(private val clickListener: (NewsItemTable)-> Unit) :
             else -> throw IllegalArgumentException()
         }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val listItem = getItem(position)
 
         holder.itemView.setOnClickListener {
@@ -56,13 +69,11 @@ class SportAdapter(private val clickListener: (NewsItemTable)-> Unit) :
 
         when (holder.itemViewType) {
             TYPE_ODD -> {
-                holder as OddViewHolder
                 Picasso.get().load(listItem.context).into(holder.image)
                 holder.headline.text = listItem.shortHeadline
                 holder.altText.text = listItem.altText
             }
             TYPE_EVEN -> {
-                holder as EvenViewHolder
                 Picasso.get().load(listItem.context).into(holder.image)
                 holder.headline.text = listItem.shortHeadline
                 holder.altText.text = listItem.altText
