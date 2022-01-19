7ceb09c1fd2e71ce8b2756b383d6f17b99618326
Agathon006 <agaton615@gmail.com>
2022-01-19 17:26:12 +0300

    First step in making square impulse...

--- compose_ui/src/main/java/com/compose_ui/SquareAnimation.kt
+++ compose_ui/src/main/java/com/compose_ui/SquareAnimation.kt
@@ -1,23 +1,28 @@
 package com.compose_ui
 
 import android.util.Log
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.calculateTargetValue
+import androidx.compose.animation.splineBasedDecay
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.*
+import androidx.compose.ui.input.pointer.util.VelocityTracker
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
-import kotlin.math.abs
-import kotlin.math.cos
-import kotlin.math.sin
-import kotlin.math.sqrt
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.*
 
 @Composable
 fun MakeAnimationSquare() {
@@ -93,6 +98,71 @@ fun MakeAnimationSquare() {
             }
             .background(Color.Green)
             .size(squareDp)
+            .impulse()
         )
     }
+}
+
+private fun Modifier.impulse(
+): Modifier = composed {
+    // TODO 6-1: Create an Animatable instance for the offset of the swiped element.
+    val offsetX = remember { Animatable(0f) } // Add this line
+    val offsetY = remember { Animatable(0f) } // Add this line
+    pointerInput(Unit) {
+        // Used to calculate a settling position of a fling animation.
+        val decay = splineBasedDecay<Float>(this)
+        // Wrap in a coroutine scope to use suspend functions for touch events and animation.
+        coroutineScope {
+            while (true) {
+                // Wait for a touch down event.
+                val pointerId = awaitPointerEventScope { awaitFirstDown().id }
+                // TODO 6-2: Touch detected; the animation should be stopped.
+                offsetX.stop() // Add this line
+                offsetY.stop()
+                // Prepare for drag events and record velocity of a fling.
+                val velocityTracker = VelocityTracker()
+                // Wait for drag events.
+                awaitPointerEventScope {
+                    drag(pointerId) { change ->
+                        // TODO 6-3: Apply the drag change to the Animatable offset.
+                        // Add these 4 lines
+                        val horizontalDragOffset = offsetX.value + change.positionChange().x
+                        val verticalDragOffset = offsetY.value + change.positionChange().y
+                        launch {
+                            offsetX.snapTo(horizontalDragOffset)
+                            offsetY.snapTo(verticalDragOffset)
+                        }
+                        // Record the velocity of the drag.
+                        velocityTracker.addPosition(change.uptimeMillis, change.position)
+                        // Consume the gesture event, not passed to external
+                        change.consumePositionChange()
+                    }
+                }
+                // Dragging finished. Calculate the velocity of the fling.
+                val velocityX = velocityTracker.calculateVelocity().x
+                val velocityY = velocityTracker.calculateVelocity().y
+                // TODO 6-4: Calculate the eventual position where the fling should settle
+                //           based on the current offset value and velocity
+                // Add this line
+                val targetOffsetX = decay.calculateTargetValue(offsetX.value, velocityX)
+                val targetOffsetY = decay.calculateTargetValue(offsetY.value, velocityY)
+                // TODO 6-5: Set the upper and lower bounds so that the animation stops when it
+                //           reaches the edge.
+                offsetX.updateBounds(
+                    lowerBound = -size.width.toFloat(),
+                    upperBound = size.width.toFloat()
+                )
+                offsetY.updateBounds(
+                    lowerBound = -size.height.toFloat(),
+                    upperBound = size.height.toFloat()
+                )
+                launch {
+                    offsetX.animateTo(targetValue = targetOffsetX, initialVelocity = velocityX)
+                    offsetX.animateTo(targetValue = targetOffsetY, initialVelocity = velocityY)
+                }
+            }
+        }
+    }
+        // Apply the offset to the element.
+        .offset { IntOffset(offsetX.value.roundToInt(), offsetY.value.roundToInt()) }
 }
