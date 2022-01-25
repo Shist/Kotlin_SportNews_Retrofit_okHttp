4821e6ac96b25b37285944f9212c443afe7f8e2e
Agathon006 <agaton615@gmail.com>
2022-01-26 00:15:29 +0300

    Little cleaning

--- compose_ui/src/main/java/com/compose_ui/SquareAnimation.kt
+++ compose_ui/src/main/java/com/compose_ui/SquareAnimation.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
@@ -26,7 +25,7 @@ fun MakeAnimationSquare() {
     val scope = rememberCoroutineScope()
     val smallBoxSize = 100.dp
     var offset by remember { mutableStateOf(Offset.Zero) }
-    var ratationAngle by remember { mutableStateOf(0f) }
+    var rotationAngle by remember { mutableStateOf(0f) }
     var scale by remember { mutableStateOf(1f) }
 
     Box(
@@ -80,7 +79,7 @@ fun MakeAnimationSquare() {
         Box(
             Modifier
                 .size(smallBoxSize)
-                .rotate(ratationAngle) // You need to pay attention to the calling sequence of offset and rotate
+                .rotate(rotationAngle) // You need to pay attention to the calling sequence of offset and rotate
                 .scale(scale)
                 .offset {
                     val halfSizePx = smallBoxSize.roundToPx() / 2
@@ -92,7 +91,7 @@ fun MakeAnimationSquare() {
                         panZoomLock = true, // Can I rotate when I pan or zoom in
                         onGesture = { centroid: Offset, pan: Offset, zoom: Float, rotation: Float ->
                             val rotatingBoundsOffset = scale * (smallBoxSize.toPx() / 2) *
-                                    (sqrt(2.0) * cos(Math.toRadians(45.0 - abs(ratationAngle % 90).toDouble())) - 1).toFloat()
+                                    (sqrt(2.0) * cos(Math.toRadians(45.0 - abs(rotationAngle % 90).toDouble())) - 1).toFloat()
                             if ((((scale * zoom - 1) / 2) * smallBoxSize.toPx() + rotatingBoundsOffset) <
                                 (size.width - smallBoxSize.toPx() * ((scale * zoom + 1) / 2) - rotatingBoundsOffset) &&
                                 (((scale * zoom - 1) / 2) * smallBoxSize.toPx() + rotatingBoundsOffset) <
@@ -102,17 +101,17 @@ fun MakeAnimationSquare() {
                                 scale *= zoom
                                 if (scale < 0.5f)
                                     scale = 0.5f
-                                ratationAngle += rotation
+                                rotationAngle += rotation
                                 val sinRotateAngle =
                                     sin(
                                         Math
-                                            .toRadians(ratationAngle.toDouble())
+                                            .toRadians(rotationAngle.toDouble())
                                             .toFloat()
                                     )
                                 val cosRotateAngle =
                                     cos(
                                         Math
-                                            .toRadians(ratationAngle.toDouble())
+                                            .toRadians(rotationAngle.toDouble())
                                             .toFloat()
                                     )
                                 val rotateRecalculatingValue = Offset(
