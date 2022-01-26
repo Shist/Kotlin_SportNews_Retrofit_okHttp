958ee9cf4eb32df308fd137b2009ec21909eb5e1
Shist <shist.pupust@mail.ru>
2022-01-26 19:26:35 +0300

    Working on physics for our square...

--- compose_ui/src/main/java/com/compose_ui/SquareAnimation.kt
+++ compose_ui/src/main/java/com/compose_ui/SquareAnimation.kt
@@ -1,8 +1,6 @@
 package com.compose_ui
 
-import android.util.Log
 import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.AnimationEndReason
 import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.exponentialDecay
 import androidx.compose.foundation.background
@@ -20,97 +18,108 @@ import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
 import kotlinx.coroutines.launch
-import kotlin.math.*
+import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 @Composable
 fun MakeAnimationSquare() {
     val squareDp = 100.dp
     var scale by remember { mutableStateOf(1f) }
-    var mRotation by remember { mutableStateOf(0f) }
-    var position = remember { Animatable(Offset(500f,500f), Offset.VectorConverter) }
-    var size by remember { mutableStateOf(Size.Zero) }
-    val scope = rememberCoroutineScope()
+    var rotationAngle by remember { mutableStateOf(0f) }
+    var fieldSize by remember { mutableStateOf(Size.Zero) }
+    val squarePosition = remember { Animatable(
+        Offset(fieldSize.width / 2,fieldSize.height / 2), Offset.VectorConverter) }
+    val scopeSizeInit = rememberCoroutineScope()
+    val scopeCalculateVelocity = rememberCoroutineScope()
+    val scopeCalculatePosition = rememberCoroutineScope()
     Box(modifier = Modifier
         .fillMaxSize()
         .onSizeChanged {
-            size = it.toSize()
-        }
+            fieldSize = it.toSize()
+            scopeSizeInit.launch {
+                squarePosition.snapTo(Offset(fieldSize.width / 2, fieldSize.height / 2))
+            }
+        } // TODO We need to put square exactly to the center
     ) {
-        Log.d("XUI", "x = " + position.value.x.toString() +
-                " ; y = " + position.value.y.toString() + "\n")
         Box(modifier = Modifier
             .graphicsLayer(
                 scaleX = scale,
                 scaleY = scale,
-                rotationZ = mRotation,
-                translationX = position.value.x,
-                translationY = position.value.y
+                rotationZ = rotationAngle,
+                translationX = squarePosition.value.x,
+                translationY = squarePosition.value.y
             )
             .pointerInput(Unit) {
                 val velocityTracker = VelocityTracker()
-                detectTransformGestures(
-                    panZoomLock = false,
-                    onGesture = { _, pan, zoom, rotation ->
-                        val rotatingBoundsOffset = scale * (squareDp.toPx() / 2) *
-                                (sqrt(2.0) * cos(Math.toRadians(45.0 - abs(mRotation % 90).toDouble())) - 1).toFloat()
-                        if ((((scale * zoom - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset) <
-                            (size.width - squareDp.toPx() * ((scale * zoom + 1) / 2) - rotatingBoundsOffset) &&
-                            (((scale * zoom - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset) <
-                            (size.height - squareDp.toPx() * ((scale * zoom + 1) / 2) - rotatingBoundsOffset)
-                        ) {
-                            scale *= zoom
-                            if (scale < 0.5f)
-                                scale = 0.5f
-                            mRotation += rotation
-                            val sinRotateAngle =
-                                sin(
-                                    Math
-                                        .toRadians(mRotation.toDouble())
-                                        .toFloat()
-                                )
-                            val cosRotateAngle =
-                                cos(
-                                    Math
-                                        .toRadians(mRotation.toDouble())
-                                        .toFloat()
-                                )
-                            val rotateRecalculatingValue = Offset(
-                                x = scale * (pan.x * cosRotateAngle - pan.y * sinRotateAngle),
-                                y = scale * (pan.y * cosRotateAngle + pan.x * sinRotateAngle),
-                            )
-                            var putInBoundsValue : Offset
-                            scope.launch {
-                                position.snapTo(position.value + rotateRecalculatingValue)
-                                putInBoundsValue = Offset(
-                                    x = position.value.x.coerceIn(
-                                        ((scale - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset,
-                                        size.width - squareDp.toPx() * ((scale + 1) / 2) - rotatingBoundsOffset
-                                    ),
-                                    y = position.value.y.coerceIn(
-                                        ((scale - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset,
-                                        size.height - squareDp.toPx() * ((scale + 1) / 2) - rotatingBoundsOffset
-                                    ),
-                                )
-                                position = Animatable(putInBoundsValue, Offset.VectorConverter)
-                                val velocity = velocityTracker.calculateVelocity()
-                                var initialVelocity = Offset(velocity.x, velocity.y)
-                                do {
-                                    val result =
-                                        position.animateDecay(initialVelocity, exponentialDecay())
-                                    initialVelocity = result.endState.velocity
-                                    if (position.value.x == position.lowerBound?.x ||
-                                        position.value.x == position.upperBound?.x ) {
-                                        initialVelocity = initialVelocity.copy(x = -initialVelocity.x)
-                                    }
-                                    if (position.value.y == position.lowerBound?.y ||
-                                        position.value.y == position.upperBound?.y ) {
-                                        initialVelocity = initialVelocity.copy(y = -initialVelocity.y)
-                                    }
-                                } while (result.endReason == AnimationEndReason.BoundReached)
+                forEachGesture {
+                    velocityTracker.resetTracking()
+                    awaitPointerEventScope {
+                        val fingerTap = awaitFirstDown()
+                        scopeCalculateVelocity.launch {
+                            squarePosition.snapTo(fingerTap.position)
+                        }
+                        drag(fingerTap.id) {
+                            velocityTracker.addPosition(it.uptimeMillis, it.position)
+                            scopeCalculateVelocity.launch {
+                                squarePosition.snapTo(it.position)
                             }
                         }
+                        val velocity = velocityTracker.calculateVelocity()
+                        scopeCalculateVelocity.launch {
+                            val initialVelocity = Offset(velocity.x, velocity.y)
+                            squarePosition.animateDecay(initialVelocity, exponentialDecay())
+                        }
                     }
-                )
+                    detectTransformGestures(
+                        panZoomLock = false,
+                        onGesture = { _, pan, zoom, rotation ->
+                            val rotatingBoundsOffset = scale * (squareDp.toPx() / 2) *
+                                    (sqrt(2.0) * cos(Math.toRadians(45.0 - abs(rotationAngle % 90).toDouble())) - 1).toFloat()
+                            if ((((scale * zoom - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset) <
+                                (fieldSize.width - squareDp.toPx() * ((scale * zoom + 1) / 2) - rotatingBoundsOffset) &&
+                                (((scale * zoom - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset) <
+                                (fieldSize.height - squareDp.toPx() * ((scale * zoom + 1) / 2) - rotatingBoundsOffset)
+                            ) {
+                                scale *= zoom
+                                if (scale < 0.5f)
+                                    scale = 0.5f
+                                rotationAngle += rotation
+                                val sinRotateAngle =
+                                    sin(
+                                        Math
+                                            .toRadians(rotationAngle.toDouble())
+                                            .toFloat()
+                                    )
+                                val cosRotateAngle =
+                                    cos(
+                                        Math
+                                            .toRadians(rotationAngle.toDouble())
+                                            .toFloat()
+                                    )
+                                val rotateRecalculatingValue = Offset(
+                                    x = scale * (pan.x * cosRotateAngle - pan.y * sinRotateAngle),
+                                    y = scale * (pan.y * cosRotateAngle + pan.x * sinRotateAngle),
+                                )
+                                scopeCalculatePosition.launch {
+                                    squarePosition.snapTo(squarePosition.value + rotateRecalculatingValue)
+                                    val putInBoundsValue = Offset(
+                                        x = squarePosition.value.x.coerceIn(
+                                            ((scale - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset,
+                                            fieldSize.width - squareDp.toPx() * ((scale + 1) / 2) - rotatingBoundsOffset
+                                        ),
+                                        y = squarePosition.value.y.coerceIn(
+                                            ((scale - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset,
+                                            fieldSize.height - squareDp.toPx() * ((scale + 1) / 2) - rotatingBoundsOffset
+                                        ),
+                                    )
+                                    squarePosition.snapTo(putInBoundsValue)
+                                }
+                            }
+                        }
+                    )
+                }
             }
             .background(Color.Green)
             .size(squareDp)
