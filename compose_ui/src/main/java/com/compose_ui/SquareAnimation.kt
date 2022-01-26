02fc4171f64b9e9b88f1c8d973867f5034ee50b8
Shist <shist.pupust@mail.ru>
2022-01-26 13:51:54 +0300

    Trying to add physics to our square...

--- compose_ui/src/main/java/com/compose_ui/SquareAnimation.kt
+++ compose_ui/src/main/java/com/compose_ui/SquareAnimation.kt
@@ -1,5 +1,10 @@
 package com.compose_ui
 
+import android.util.Log
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationEndReason
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.exponentialDecay
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
@@ -10,29 +15,29 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.*
+import androidx.compose.ui.input.pointer.util.VelocityTracker
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
-import kotlin.math.abs
-import kotlin.math.cos
-import kotlin.math.sin
-import kotlin.math.sqrt
+import kotlinx.coroutines.launch
+import kotlin.math.*
 
 @Composable
 fun MakeAnimationSquare() {
     val squareDp = 100.dp
     var scale by remember { mutableStateOf(1f) }
     var mRotation by remember { mutableStateOf(0f) }
-    var offset by remember { mutableStateOf(Offset.Zero) }
+    var position = remember { Animatable(Offset(500f,500f), Offset.VectorConverter) }
     var size by remember { mutableStateOf(Size.Zero) }
+    val scope = rememberCoroutineScope()
     Box(modifier = Modifier
         .fillMaxSize()
         .onSizeChanged {
             size = it.toSize()
-            offset = Offset(
+            position = Animatable(Offset(
                 x = size.width / 2, // TODO We need to put square exactly to the center
                 y = size.height / 2
-            ) // TODO We need to put square exactly to the center
+            ), Offset.VectorConverter) // TODO We need to put square exactly to the center
         }
     ) {
         Box(modifier = Modifier
@@ -40,13 +45,15 @@ fun MakeAnimationSquare() {
                 scaleX = scale,
                 scaleY = scale,
                 rotationZ = mRotation,
-                translationX = offset.x,
-                translationY = offset.y
+                translationX = position.value.x,
+                translationY = position.value.y
             )
             .pointerInput(Unit) {
+                val velocityTracker = VelocityTracker()
                 detectTransformGestures(
                     panZoomLock = false,
                     onGesture = { _, pan, zoom, rotation ->
+                        velocityTracker.resetTracking()
                         val rotatingBoundsOffset = scale * (squareDp.toPx() / 2) *
                                 (sqrt(2.0) * cos(Math.toRadians(45.0 - abs(mRotation % 90).toDouble())) - 1).toFloat()
                         if ((((scale * zoom - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset) <
@@ -74,18 +81,38 @@ fun MakeAnimationSquare() {
                                 x = scale * (pan.x * cosRotateAngle - pan.y * sinRotateAngle),
                                 y = scale * (pan.y * cosRotateAngle + pan.x * sinRotateAngle),
                             )
-                            offset += rotateRecalculatingValue
-                            val putInBoundsValue = Offset(
-                                x = offset.x.coerceIn(
-                                    ((scale - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset,
-                                    size.width - squareDp.toPx() * ((scale + 1) / 2) - rotatingBoundsOffset
-                                ),
-                                y = offset.y.coerceIn(
-                                    ((scale - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset,
-                                    size.height - squareDp.toPx() * ((scale + 1) / 2) - rotatingBoundsOffset
-                                ),
-                            )
-                            offset = putInBoundsValue
+                            var putInBoundsValue : Offset
+                            Log.d("XUI", "x = " + position.value.x.toString() +
+                                    " ; y = " + position.value.y.toString() + "\n")
+                            scope.launch {
+                                position.snapTo(position.value + rotateRecalculatingValue)
+                                putInBoundsValue = Offset(
+                                    x = position.value.x.coerceIn(
+                                        ((scale - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset,
+                                        size.width - squareDp.toPx() * ((scale + 1) / 2) - rotatingBoundsOffset
+                                    ),
+                                    y = position.value.y.coerceIn(
+                                        ((scale - 1) / 2) * squareDp.toPx() + rotatingBoundsOffset,
+                                        size.height - squareDp.toPx() * ((scale + 1) / 2) - rotatingBoundsOffset
+                                    ),
+                                )
+                                position = Animatable(putInBoundsValue, Offset.VectorConverter)
+                                val velocity = velocityTracker.calculateVelocity()
+                                var initialVelocity = Offset(velocity.x, velocity.y)
+                                do {
+                                    val result =
+                                        position.animateDecay(initialVelocity, exponentialDecay())
+                                    initialVelocity = result.endState.velocity
+                                    if (position.value.x == position.lowerBound?.x ||
+                                        position.value.x == position.upperBound?.x ) {
+                                        initialVelocity = initialVelocity.copy(x = -initialVelocity.x)
+                                    }
+                                    if (position.value.y == position.lowerBound?.y ||
+                                        position.value.y == position.upperBound?.y ) {
+                                        initialVelocity = initialVelocity.copy(y = -initialVelocity.y)
+                                    }
+                                } while (result.endReason == AnimationEndReason.BoundReached)
+                            }
                         }
                     }
                 )
