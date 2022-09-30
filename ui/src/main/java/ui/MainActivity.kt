50608000976202f4f5697eb3b5fd47ab816ad636
Shist <shist.pupust@mail.ru>
2022-09-30 14:44:47 +0300

    Added navigation menu and app creator's contacts...

--- ui/src/main/java/ui/MainActivity.kt
+++ ui/src/main/java/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package ui
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.ui.AppBarConfiguration
 import ui.databinding.ActivityMainBinding
 import ui.fragments.NewsListFragment
 import ui.fragments.NewsPageFragment
@@ -9,6 +13,7 @@ import ui.fragments.NewsPageFragment
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +22,40 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val drawerLayout = binding.drawerLayout
+        val navView = binding.navView
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.nav_news -> {
+
+                }
+                R.id.nav_contacts -> {
+
+                }
+            }
+            true
+        }
+
         if (savedInstanceState == null)
         {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, NewsListFragment())
                 .commit()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun onItemClick(itemID: String) {
